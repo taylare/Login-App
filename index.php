d60e06777ebfcc0d082a8fd002cd30eafaf8efa1
Tayla Rechichi <tay.rechichi32@gmail.com>
2025-03-26 22:16:13 -0700

    Initialize MySQL connection using MySQLi and add basic homepage structure

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+
+$con = mysqli_connect("localhost", "root", "");
+
+if ($con){
+    echo "connected";
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login App with SQL & PHP</title>
+</head>
+<body>
+    <h2>Welcome to the homepage!</h2>
+</body>
+</html>
