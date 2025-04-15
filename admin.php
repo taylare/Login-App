f8828b84af1045bdd4cacb06af2c6219b0f9551e
Tayla Rechichi <tay.rechichi32@gmail.com>
2025-04-14 21:33:44 -0700

    Enabled editing of usernames, added session flash messages for edit/delete actions, and ensured updated user list is fetched

--- admin.php
+++ admin.php
@@ -6,25 +6,67 @@
         redirect("login.php");
     }
 
-    $result = mysqli_query($conn, "SELECT id, username, email, reg_date FROM users;");
-    
+    // Editing user emails:
     if ($_SERVER["REQUEST_METHOD"] === "POST") {
-        if (isset($_POST["edit_user"])) { //check if edit is clicked
+        if (isset($_POST["edit_user"])) {
             $user_id = mysqli_real_escape_string($conn, $_POST["user_id"]);
-            $new_email = mysqli_real_escape_string($conn, $_POST["email"]); 
+            $new_email = mysqli_real_escape_string($conn, $_POST["email"]);
+            $new_username = mysqli_real_escape_string($conn, $_POST["username"]); 
 
-            $sql = "UPDATE users SET email = '$new_email' WHERE id = $user_id";
-            mysqli_query($conn, $sql);
-            redirect("admin.php");
+            $sql = "UPDATE users SET email = '$new_email', username = '$new_username' WHERE id = $user_id";
+            $result = mysqli_query($conn, $sql);
+           
+           //notification
+            $query_status = check_query($result);
+            
+            if ($query_status === true) {
+
+                $_SESSION['message'] = "user updated successfully to {$new_username}, {$new_email}";
+                $_SESSION['msg_type'] = "success";
+
+
+                redirect("admin.php");
+            }
+        } 
+        elseif (isset($_POST["delete_user"])) { // Deleting users:
+            $user_id = mysqli_real_escape_string($conn, $_POST["user_id"]); 
+
+            $sql = "DELETE FROM users WHERE id = $user_id";
+            $result = mysqli_query($conn, $sql);
+            //notification
+            $query_status = check_query($result);
+            
+            if ($query_status === true) {
+
+                $_SESSION['message'] = "user deleted successfully, record with id: {$user_id}";
+                $_SESSION['msg_type'] = "error";
+
+                redirect("admin.php");
+            }
         }
     }
- 
- ?>
+
+    //  fetch the updated list:
+    $result = mysqli_query($conn, "SELECT id, username, email, reg_date FROM users;");
+?>
 
     <h1> Welcome to admin <?php echo $_SESSION['username'] ?>!</h1>
     <h1>Manage Users</h1>
 
 <div class="container">
+
+    <?php if (isset($_SESSION['message'])): ?> <!--checking whether a message exists in the session before showing it -->
+
+        <div class="notification <?php echo $_SESSION['msg_type']?>">
+            <?php 
+                echo $_SESSION['message']; 
+                unset($_SESSION['message']);
+                unset($_SESSION['msg_type']);
+            ?>
+        </div>
+
+
+     <?php endif; ?>   
     <table class="user-table">
         <thead>
         <tr>
@@ -45,6 +87,7 @@
         <td><?php echo readableDate($user['reg_date']) ?></td>
         <td><form method="POST" style="display:inline-block;">
                     <input type="hidden" name="user_id" value="<?php echo $user['id'] ?>">
+                    <input type="text" name="username" value="<?php echo $user['username'] ?>" required>
                     <input type="email" name="email" value="<?php echo $user['email'] ?>" required>
                     <button class="edit" type="submit" name="edit_user">Edit</button>
                 </form>
--- db.php
+++ db.php
@@ -1,5 +1,18 @@
 <?php
 
-$conn = mysqli_connect("localhost", "root", "", "login_app");
+$host = "localhost";
+$username = "root";
+$password = "";
+$db = "login-app";
+
+
+$conn = mysqli_connect($host, $username, $password, $db);
+function check_query($result){
+    global $conn;
+    if(!$result){
+        return "Error" . mysqli_error($conn);
+    }
+    return true;
+}
 
 ?>
